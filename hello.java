293fa1c00db098f62301778e5b3697ab4d1b7b63
Akshaykumar9640 <159756603+Akshaykumar9640@users.noreply.github.com>
2024-02-13 20:36:02 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+package com.smsk;
+
+public class hello {
+	public static void main(String[] args) {
+		//print a greeting message to the console
+		System.out.println("Hello\nShyamala Akshay!");
+	}
+
+}
