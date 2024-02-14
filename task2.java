b95b22f39158e3d5ec1865ce7f66ee272ecec690
Akshaykumar9640 <159756603+Akshaykumar9640@users.noreply.github.com>
2024-02-14 08:25:26 +0530

    Add files via upload

---
+++ task2.java
@@ -0,0 +1,16 @@
+package tasks.smsk;
+
+public class task2 {
+	public static void main(String[] args) {
+		int sum = 23+22;
+		System.out.println(sum);
+		int sub = 23-22;
+		System.out.println(sub);
+		int result = 23/22;
+		System.out.println(result);
+		int mult = 15*3;
+		System.out.println(mult);
+		
+	}
+
+}
